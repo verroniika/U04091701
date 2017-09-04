ac27e3ad5c5cd388995d94701c88b50b16446b15
unknown <veronika@ranta.com>
2017-09-04 12:47:47 +0200

    Första uppgiften påbörjad men behöver justeras

---
+++ U04091701/U04091701/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace U04091701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
